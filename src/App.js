59dd95dd13ffbb48c4d169e009b79b1acd2dfedd
salogo <zakariasalgo@yahoo.fr>
2021-04-25 16:33:54 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import TopNav from "./components/TopNav";
 import Home from "./booking/Home";
 import Longin from "./auth/Login";
 import Register from "./auth/Register";
 
+
+
 function App() {
   return (
     <BrowserRouter>
+      <TopNav />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Longin} />
---
+++ src/components/TopNav.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+const TopNav = () => (
+    <div className="nav bg-light d-flex justify-content-between">
+        <Link className="nav-link" to="/" >
+            Home
+      </Link>
+        <Link className="nav-link" to="/login" >
+            Login
+      </Link>
+        <Link className="nav-link" to="register">
+            Register
+      </Link>
+    </div>
+);
+export default TopNav;
